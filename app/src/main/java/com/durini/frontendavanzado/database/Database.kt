254866f8f55cb9cda010278c5c5b89f4b0c5d296
GonzaloSantizo <84192252+GonzaloSantizo@users.noreply.github.com>
2022-09-21 17:01:28 -0600

    tercer entregable
    made changes to Database, added intial muscle types

--- app/src/main/java/com/durini/frontendavanzado/database/Database.kt
+++ app/src/main/java/com/durini/frontendavanzado/database/Database.kt
@@ -11,74 +11,61 @@ object Database {
     private val places  = mutableListOf(
         Place(
             name = "Bicep",
-            status = "Alive",
-            species = "Human",
-            gender = "Male",
+            status = "Ejercicio1: Curls con barra",
+            species = "Ejercicio 2: Curls con mancuerna",
+            gender = "Ejercicio 3: Remo inclinado",
             type = PlaceType.Human
         ),
         Place(
             name = "Tricep",
-            status = "Alive",
-            species = "Human",
-            gender = "Male",
+            status = "Ejercicio1: Flexiones con manos juntas",
+            species = "Ejercicio 2: Extenciones en TRX",
+            gender = "Ejercicio 3: Press Fr*nces",
             type = PlaceType.Human
         ),
         Place(
             name = "Gluteos",
-            status = "Alive",
-            species = "Human",
-            gender = "Female",
+            status = "Ejercicio1: peso muerto",
+            species = "Ejercicio 2: Frog Pump",
+            gender = "Ejercicio 3: Barbell hip thrust",
             type = PlaceType.Human
         ),
         Place(
             name = "Abdominales",
-            status = "Alive",
-            species = "Human",
-            gender = "Female",
+            status = "Ejercicio1: Crunches",
+            species = "Ejercicio 2: Sit ups",
+            gender = "Ejercicio 3: Russian Twist",
             type = PlaceType.Human
         ),
         Place(
             name = "Deltoides",
-            status = "Alive",
-            species = "Human",
-            gender = "Male",
+            status = "Ejercicio1: Press de hombro",
+            species = "Ejercicio 2: levantamiento frontal",
+            gender = "Ejercicio 3: Encogimiento de hombro",
             type = PlaceType.Human
         ),
         Place(
-            name = "Abadango Cluster Princess",
-            status = "Alive",
-            species = "Alien",
-            gender = "Female",
+            name = "Pecho",
+            status = "Ejercicio1: Push ups",
+            species = "Ejercicio 2: Press de banca",
+            gender = "Ejercicio 3: Flexion de arquero",
             type = PlaceType.Alien
         ),
         Place(
-            name = "Abradolf Lincler",
-            status = "unknown",
-            species = "Human",
-            gender = "Male",
+            name = "Pierna",
+            status = "Ejercicio1: Sentadilla",
+            species = "Ejercicio 2: Sentadilla con barra",
+            gender = "Ejercicio 3: Sentadilla frontal",
             type = PlaceType.Human
         ),
         Place(
-            name = "Adjudicator Rick",
-            status = "Dead",
-            species = "Human",
-            gender = "Male",
+            name = "Hombro",
+            status = "Ejercicio1: Press de hombro",
+            species = "Ejercicio 2: levantamiento frontal",
+            gender = "Ejercicio 3: Encogimiento de hombro",
             type = PlaceType.Human
         ),
-        Place(
-            name = "Agency Director",
-            status = "Dead",
-            species = "Human",
-            gender = "Male",
-            type = PlaceType.Alien
-        ),
-        Place(
-            name = "Alan Rails",
-            status = "Dead",
-            species = "Human",
-            gender = "Male",
-            type = PlaceType.Human
-        ),
+
     )
 
     fun getPlaces() = places
