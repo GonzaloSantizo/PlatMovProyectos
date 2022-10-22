1af65a014cf4c3e9589b643aaa88b829870fdadb
Samuel Argueta <arg211024@uvg.edu.gt>
2022-10-21 21:07:00 -0600

    Pantallas

---
+++ app/src/main/java/com/fitness/project/ui/fragments/LogInFragment.kt
@@ -0,0 +1,27 @@
+package com.fitness.project.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.fitness.project.R
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LogInFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LogInFragment : Fragment() {
+
+}
