c85e6aa61caaa300502162c82ac01c6b51ee123c
Samuel Argueta <arg211024@uvg.edu.gt>
2022-10-21 19:28:58 -0600

    Start basic

--- app/src/main/java/com/durini/frontendavanzado/fragments/DetailsFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.durini.frontendavanzado.fragments
-
-import androidx.fragment.app.Fragment
-import com.durini.frontendavanzado.R
-
-class DetailsFragment : Fragment(R.layout.fragment_details) {
-
-}
