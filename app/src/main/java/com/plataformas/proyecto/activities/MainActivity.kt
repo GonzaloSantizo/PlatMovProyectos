5367e085e943852972bb48bec20737339b4b058a
Jose Andres Auyon <josea13p@gmail.com>
2022-10-27 08:02:21 -0600

    Vista registro añadida y navigation de vistas

--- app/src/main/java/com/plataformas/proyecto/activities/MainActivity.kt
+++ app/src/main/java/com/plataformas/proyecto/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.plataformas.proyecto
+package com.plataformas.proyecto.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.plataformas.proyecto.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/plataformas/proyecto/fragments/LogInFragment.kt
+++ app/src/main/java/com/plataformas/proyecto/fragments/LogInFragment.kt
@@ -7,17 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 import com.plataformas.proyecto.R
 
 
 class LogInFragment : Fragment(R.layout.fragment_log_in) {
 private lateinit var btnLog:Button
+private lateinit var textViewRegistro : TextView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.apply {
             btnLog = findViewById(R.id.btnIniciar)
+            textViewRegistro = findViewById(R.id.textViewRegistro)
         }
 
 
@@ -27,6 +30,9 @@ private lateinit var btnLog:Button
     private fun setListeners() {
         btnLog.setOnClickListener {
             requireView().findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToMuscleListFragment())
+        }
+        textViewRegistro.setOnClickListener{
+            requireView().findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToRegisterFragment())
         }
     }
 
