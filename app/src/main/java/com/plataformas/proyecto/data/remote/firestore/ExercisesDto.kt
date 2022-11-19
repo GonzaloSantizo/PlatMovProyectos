b50599b3c2a2fb0bd482ce3924a1c970162a0a04
Samuel Argueta <arg211024@uvg.edu.gt>
2022-11-19 15:57:43 -0600

    Modificiones extra

--- app/src/main/java/com/plataformas/proyecto/data/remote/firestore/ExercisesDto.kt
+++ app/src/main/java/com/plataformas/proyecto/data/remote/firestore/ExercisesDto.kt
@@ -3,4 +3,5 @@ package com.plataformas.proyecto.data.remote.firestore
 data class ExercisesDto(
     var excercise: String ?= null,
     var description: String ?= null,
-    var image : String ?= null )
+    var image : String ?= null
+)
